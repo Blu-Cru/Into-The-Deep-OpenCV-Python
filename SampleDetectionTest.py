9f85ec60ccc6a491e26f7644b247a1a008660079
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2025-02-18 19:56:05 -0500

    laaaaaaaarjelkjr

--- SampleDetectionTest.py
+++ SampleDetectionTest.py
@@ -1,55 +1,104 @@
 import cv2
 import numpy as np
 
-src = cv2.imread(r"images\1.jpg")
-if src is None:
-    print("Error: image not loaded")
+def main():
+    src = cv2.imread(r"images\1.jpg")
+    if src is None:
+        print("Error: image not loaded")
 
-# cv2.imshow("Src", src)
+    show("Src", src)
 
-rotated = cv2.rotate(src, cv2.ROTATE_180)
+    hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
-hsv = cv2.cvtColor(rotated, cv2.COLOR_BGR2HSV)
+    #dilation
+    # dilationElement = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+    # dilated = cv2.dilate(src, dilationElement)
+    # cv2.imshow("dilated", dilated)
 
-#dilation
-dilationElement = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-dilated = cv2.dilate(rotated, dilationElement)
-# cv2.imshow("dilated", dilated)
+    # erosion
+    # erosionElement = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
 
-# erosion
-erosionElement = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+    # dilatedEroded = cv2.erode(dilated, erosionElement)
+    # show("Both", dilated)
 
-# eroded = cv2.erode(src, element)
-# cv2.imshow("Eroded", eroded)
+    color = 1 # 0 for red, 1 for yellow, 2 for blue
 
-# edges = cv2.Canny(src, 100, 200)
-# cv2.imshow("edges", edges)
+    if color == 0:
+        thresh1 = cv2.inRange(hsv, (0, 60, 20), (15, 255, 255))
+        thresh2 = cv2.inRange(hsv, (150, 60, 20), (180, 255, 255))
+        hsvThresh = cv2.bitwise_or(thresh1, thresh2)
+    elif color == 1:
+        hsvThresh = cv2.inRange(hsv, (15, 100, 20), (70, 255, 255))
+    else:
+        hsvThresh = cv2.inRange(hsv, (80, 40, 20), (150, 255,255))
 
-dilatedEroded = cv2.erode(dilated, erosionElement)
-cv2.imshow("Both", dilatedEroded)
 
-color = 2 # 0 for red, 1 for yellow, 2 for blue
+    masked = cv2.bitwise_and(src, src, mask=hsvThresh)
+    show("masked", masked)
 
-if color == 0:
-    thresh1 = cv2.inRange(hsv, (0, 60, 20), (10, 255, 255))
-    thresh2 = cv2.inRange(hsv, (150, 60, 20), (180, 255, 255))
-    hsvThresh = cv2.bitwise_or(thresh1, thresh2)
-elif color == 1:
-    hsvThresh = cv2.inRange(hsv, (10, 100, 20), (70, 255, 255))
-else:
-    hsvThresh = cv2.inRange(hsv, (80, 110, 20), (140, 255,255))
+    blurred = cv2.GaussianBlur(masked, (5, 5), 1)
 
-masked = cv2.bitwise_and(dilatedEroded, dilatedEroded, mask=hsvThresh)
-cv2.imshow("masked", masked)
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    equalized = clahe.apply(gray)
 
-edges = cv2.Canny(masked, 100, 200)
-cv2.imshow("new edges", edges)
+    edges = cv2.Canny(equalized, 50, 100)
+    show("new edges", edges)
 
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-contourImage = np.zeros_like(src)
-cv2.drawContours(contourImage, contours, -1, (0, 255, 0), 2)
-cv2.imshow("new contours", contourImage)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contourImage = np.zeros_like(src)
+    cv2.drawContours(contourImage, contours, -1, (0, 255, 0), 2)
+    show("new contours", contourImage)
 
-cv2.waitKey(0)
+    cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+def show(str, mat):
+    cv2.imshow(str, cv2.resize(mat, (960, 540)))
+
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+    
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+    
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+    
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+    
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+    
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+    
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
+
+main()
